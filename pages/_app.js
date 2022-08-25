9763b8475dfb4301a7da5e211b3b8dd1d7434e86
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-25 23:03:05 +0300

    pages are initialized and classes are defined

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+   return (
+      <>
+         <Head>
+            <title>Marvel Characters</title>
+            <meta name='description' content='A practice project with marvel api' />
+            <link rel='icon' href='/favicon.ico' />
+         </Head>
+         <Component {...pageProps} />
+      </>
+   );
 }
-
-export default MyApp
---
+++ utils/character.js
@@ -0,0 +1,9 @@
+export class Character {
+   constructor(id, name, desc, thumbnail, comics) {
+      this.id = id;
+      this.name = name;
+      this.description = desc;
+      this.thumbnail = thumbnail;
+      this.comics = comics;
+   }
+}
