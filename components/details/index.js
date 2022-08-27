abc19ac087afdf44cfdbca4bdf97977e782b6274
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-27 14:35:34 +0300

    worked on styles

--- components/details/index.js
+++ components/details/index.js
@@ -4,17 +4,19 @@ import styles from './styles.module.scss';
 export default function Details(props) {
    const { name, description, thumbnail, comics, isLoading } = props.config;
    const { path, extension } = thumbnail;
-
-   console.log('details comp', props.config);
+   const imageURL = `${path}.${extension}`;
 
    return (
       <div className={styles.container}>
-         <img src={`${path}.${extension}`} />
+         <h1>{name}</h1>
+         {description && <h3>{description}</h3>}
          <div className={styles.group}>
-            <h2>{name}</h2>
-            <div>{description}</div>
-            {isLoading && <div>Loading..</div>}
-            {!isLoading && comics?.map(comic => <Card key={comic.id} config={comic} comic />)}
+            <img src={imageURL} onClick={() => window.open(imageURL, '_blank')} />
+            <div className={styles['sub-group']}>
+               <h2>Comics</h2>
+               {isLoading && <div>Loading..</div>}
+               {!isLoading && comics?.map(comic => <Card key={comic.id} config={comic} comic />)}
+            </div>
          </div>
       </div>
    );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import { fetchCharacters } from './api/utils/functions';
 import Home from '../components/home';
-// import Card from '../components/card';
 
 export default function HomePage(props) {
    const { characters } = props;
-
-   // return <Card config={{ id: 1, title: 'Erdem', description: 'Erdem Ceylan', thumbnail: { path: 'hi', extension: 'jpg' } }} comic />;
-   // return <Card config={{ id: 1, name: 'Erdem', description: 'Erdem Ceylan', thumbnail: { path: 'hi', extension: 'jpg' } }} />;
 
    return <Home initialCharacters={characters} />;
 }
