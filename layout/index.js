6b259f5ba3143ed8a70727bfbd621d00c4fd1bdf
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-28 04:30:44 +0300

    header is complete

--- layout/index.js
+++ layout/index.js
@@ -1,9 +1,13 @@
 import styles from './styles.module.scss';
+import Header from '../components/header';
 
 export default function Layout(props) {
    return (
-      <div className={styles.container}>
-         {props.children}
-      </div>
+      <>
+         <Header />
+         <div className={styles.container}>
+            {props.children}
+         </div>
+      </>
    );
 }
